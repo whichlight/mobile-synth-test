bb4ffa35a625ff09ad4d0a444f010038cb3f1f3d
Kawandeep Virdee <whichlight@gmail.com>
2014-04-02 00:36:18 -0400

    added filter for vertical tilt

--- app.js
+++ app.js
@@ -20,17 +20,30 @@ if (window.DeviceMotionEvent) {
     document.getElementById("dmEvent").innerHTML = "Not supported."
 }
 
+if (window.DeviceOrientationEvent) {
+  // Listen for the event and handle DeviceOrientationEvent object
+  window.addEventListener('deviceorientation', devOrientHandler, false);
+}
+
+function map_range(value, low1, high1, low2, high2) {
+    return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
+}
 
 var accelControl;
 
 oscillator = context.createOscillator(); // Create sound source
 oscillator.type = 2; // Square wave
 oscillator.frequency.value = 400
+var filter = context.createBiquadFilter();
+
 
 gainNode = context.createGainNode();
 gainNode.gain.value = 0;
+filter.type = 0;
+filter.frequency.value = 440;
 
-oscillator.connect(gainNode); // Connect sound to output
+oscillator.connect(filter); // Connect sound to output
+filter.connect(gainNode);
 gainNode.connect(context.destination);
 oscillator.active = false;
 
@@ -91,8 +104,27 @@ function deviceMotionHandler(eventData) {
   // // Grab the refresh interval from the results
   info = eventData.interval;
 
-
   var accelControl = acceleration.x;
   oscillator.frequency.value = 200+ accelControl*50;
 
 }
+
+function devOrientHandler(eventData) {
+    // gamma is the left-to-right tilt in degrees, where right is positive
+    var tiltLR = eventData.gamma;
+
+    // beta is the front-to-back tilt in degrees, where front is positive
+    var tiltFB = eventData.beta;
+    var filterval = map_range(tiltFB, -90, 90, 10000, 0);
+
+    filter.frequency.value = filterval;
+
+
+    // alpha is the compass direction the device is facing in degrees
+    var dir = eventData.alpha;
+
+    // call our orientation event handler
+    deviceOrientationHandler(tiltLR, tiltFB, dir);
+}
+
+
