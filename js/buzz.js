8ecd36ab5afcf566b943e920018eb0799345e2d1
Kawandeep Virdee <whichlight@gmail.com>
2014-04-13 22:15:17 -0400

    added graphic accelerometer stuff

--- js/buzz.js
+++ js/buzz.js
@@ -68,10 +68,12 @@ var touchDeactivate = function(e){
 
 function deviceMotionHandler(eventData) {
   synth.accelHandler(eventData);
+  graphic.accelHandler(eventData);
 }
 
 function devOrientHandler(eventData) {
   synth.orientHandler(eventData);
+  graphic.orientHandler(eventData);
 }
 
 
@@ -84,7 +86,7 @@ function Note(){
   this.filter;
   this.gain;
   this.osc;
-  this.active = false;
+  this.played = false;
   this.buildSynth();
 }
 
@@ -120,11 +122,11 @@ Note.prototype.setVolume= function(v){
 
 Note.prototype.play = function(e){
   e.preventDefault();
-  if(!this.active){
+  if(!this.played){
     this.osc.noteOn(0); // Play instantly
   }
 
-  this.active = true;
+  this.played = true;
   this.setVolume(0.5);
   return false;
 }
@@ -162,16 +164,62 @@ Synth.prototype.orientHandler = function(orient){
 
 
 function Graphic(){
-
+  this.activated = false;;
+  this.background_color="purple";
 }
 
 Graphic.prototype.touchActivate = function(e){
-  $fun.css("background-color","lime");
+  this.activated = true;
+  $fun.css("background-color", "lime");
+  $fun.css("background-color", this.background_color);
 }
 
 Graphic.prototype.touchDeactivate = function(e){
+  this.activated = false;
   $fun.css("background-color","white");
 }
 
+Graphic.prototype.accelHandler = function(accel){
+ var x = accel.accelerationIncludingGravity.x;
+ var h = x/60.0;
+ var c  = HSVtoRGB(h,1,1);
+ this.background_color = "rgb("+c.r+","+c.g+","+c.b+")" ;
+ if(this.activated){
+   $fun.css("background-color", this.background_color);
+ }
+}
+
+Graphic.prototype.orientHandler = function(orient){
+}
+
+/* accepts parameters
+ * h  Object = {h:x, s:y, v:z}
+ * OR
+ * h, s, v
+*/
+function HSVtoRGB(h, s, v) {
+    var r, g, b, i, f, p, q, t;
+    if (h && s === undefined && v === undefined) {
+        s = h.s, v = h.v, h = h.h;
+    }
+    i = Math.floor(h * 6);
+    f = h * 6 - i;
+    p = v * (1 - s);
+    q = v * (1 - f * s);
+    t = v * (1 - (1 - f) * s);
+    switch (i % 6) {
+        case 0: r = v, g = t, b = p; break;
+        case 1: r = q, g = v, b = p; break;
+        case 2: r = p, g = v, b = t; break;
+        case 3: r = p, g = q, b = v; break;
+        case 4: r = t, g = p, b = v; break;
+        case 5: r = v, g = p, b = q; break;
+    }
+    return {
+        r: Math.floor(r * 255),
+        g: Math.floor(g * 255),
+        b: Math.floor(b * 255)
+    };
+}
 
 
